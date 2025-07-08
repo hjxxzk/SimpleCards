1e7a4a508e9db4c457503db8f31b5eea9fffc780
agnieszka <273017@student.pwr.edu.pl>
2025-07-08 10:08:12 +0200

    hide scrollbar

--- src/components/Bars/Deck/Deck.styles.ts
+++ src/components/Bars/Deck/Deck.styles.ts
@@ -1,10 +1,10 @@
 const useStyles = () => {
     return {
-        deck: "bg-white p-4 rounded-lg w-full h-38 flex flex-col justify-center items-center group relative overflow-hidden my-4",
+        deck: "bg-white p-4 rounded-lg w-full h-38 flex flex-col justify-center items-center group relative overflow-hidden my-4 hover:shadow-lg transition-shadow duration-300 hover:border-2 hover:border-amber-300",
         deckHeader: "flex justify-between items-center w-full",
         deckActions: "flex justify-between items-center gap-1",
         icon: "cursor-pointer hover:stroke-gray-700 active:stroke-gray-700 transition-colors md:max-3xl:invisible md:max-3xl:group-hover:visible ",
-        separator: "my-1 h-0.5 border-t-0 bg-amber-200 w-full",
+        separator: "my-1 h-0.5 border-t-0 bg-black w-full",
         description: "line-clamp-2 mb-2 w-full flex-1",
         language: "flex justify-end items-center w-full",
     };
--- src/components/Bars/Sidebar/Sidebar.styles.ts
+++ src/components/Bars/Sidebar/Sidebar.styles.ts
@@ -1,8 +1,8 @@
 const useStyles = () => {
     return {
-        sidebar: "bg-gray-100 py-10 px-15 lg:pt-5 pb-25 lg:px-20 w-full lg:w-120 h-full flex flex-col",
+        sidebar: "bg-gray-100 py-10 px-15 lg:pt-5 pb-25 lg:px-15 w-full lg:w-100 h-full flex flex-col",
         separator: "my-1 h-0.5 border-t-0 bg-black mt-2 mb-7",
-        decksList: "flex-1 overflow-auto items-center mb-15",
+        decksList: "flex-1 overflow-auto scrollbar-hidden pr-4 items-center mb-15",
     };
 };
 
