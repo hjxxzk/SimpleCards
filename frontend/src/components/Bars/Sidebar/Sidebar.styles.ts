bec3d72d5039c0c4c6fb54db877b1eadc108f08f
agnieszka <273017@student.pwr.edu.pl>
2025-07-17 08:19:18 +0200

    edit languagesField responsiveness

--- frontend/src/components/Bars/Sidebar/Sidebar.styles.ts
+++ frontend/src/components/Bars/Sidebar/Sidebar.styles.ts
@@ -1,6 +1,6 @@
 const useStyles = () => {
     return {
-        sidebar: "bg-gray-100 py-5 pb-20 px-15 md:px-40 lg:px-5 xl:px-10 lg:w-1/5 overflow-hidden absolute w-screen lg:static h-screen flex flex-col z-1000",
+        sidebar: "bg-gray-100 py-5 pb-20 px-15 md:px-40 lg:px-5 xl:px-10 lg:w-1/5 3xl:w-1/6 overflow-hidden absolute w-screen lg:static h-screen flex flex-col z-1000",
         separator: "h-0.5 border-t-0 bg-black mt-2 mb-7",
         decksList: "flex-1 overflow-auto scrollbar-hidden items-center lg:mb-15",
         text: "text-gray-600 text-md font-semibold text-center m-5",
--- frontend/src/components/EditView/EditView.styles.ts
+++ frontend/src/components/EditView/EditView.styles.ts
@@ -1,6 +1,6 @@
 const useStyles = () => {
     return {
-        mainContainer: "w-full lg:w-4/5 bg-white flex flex-col lg:flex-row rounded-tl-3xl shadow-lg mb-5 my-3 mx-5 lg:my-5 lg:mr-10 rounded-3xl",
+        mainContainer: "w-full lg:w-4/5 3xl:w-5/6 bg-white flex flex-col lg:flex-row rounded-tl-3xl shadow-lg mb-5 my-3 mx-5 lg:my-5 lg:mr-10 rounded-3xl",
     };
 }
 
