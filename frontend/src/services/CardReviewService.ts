62838c15cf97f7c87ca2bfb9a57cbcefe03678df
agnieszka <273017@student.pwr.edu.pl>
2025-07-21 18:31:34 +0200

    add vitest and review algorithm

--- frontend/src/services/CardReviewService.ts
+++ frontend/src/services/CardReviewService.ts
@@ -7,3 +7,37 @@ export function shuffleCards(cards: CardProps[]) {
     }
     return cards;
 }
+
+const MINIMUM_NUMBER_OF_CARDS_TO_SWAP = 3;
+
+
+export function addCardToRepeatInRandomPlace(cardToRepeat: CardProps, cards: CardProps[]): CardProps[] {
+
+    const cardToRepeatIndex = cards.lastIndexOf(cardToRepeat);
+    const middleOfCardsLeftToReview = cardToRepeatIndex + Math.ceil((cards.length - cardToRepeatIndex) / 2);
+    const newRandomIndex = returnRandomIndexBetweenGivenEdgepoints(middleOfCardsLeftToReview, cards.length);
+
+    cards.push(cardToRepeat);
+
+    if (areThereEnoughCardsToSwapPlaces(cardToRepeatIndex, cards)) {
+        return swapCardsToPutRepeatedCardOnNewRandomIndex(newRandomIndex, cards);
+    } else {
+        return cards;
+    }
+}
+
+function swapCardsToPutRepeatedCardOnNewRandomIndex(chosenIndex: number, cards: CardProps[]): CardProps[] {
+    for (let i = cards.length - 1; i > chosenIndex; i--) {
+        [cards[i], cards[i - 1]] = [cards[i - 1], cards[i]];
+    }
+    return cards;
+}
+
+function areThereEnoughCardsToSwapPlaces(cardToRepeatIndex: number, cards: CardProps[]) {
+    const numberOfCardsLeftToRepeat = (cards.length - 1) - cardToRepeatIndex;
+    return numberOfCardsLeftToRepeat >= MINIMUM_NUMBER_OF_CARDS_TO_SWAP;
+}
+
+export function returnRandomIndexBetweenGivenEdgepoints(firstEdgepoint: number, secondEdgepoint: number) {
+    return Math.round(Math.random() * (secondEdgepoint - firstEdgepoint + 1) + firstEdgepoint);
+}
