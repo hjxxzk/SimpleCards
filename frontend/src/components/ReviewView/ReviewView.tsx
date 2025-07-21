d6f4d56d9c0616233ea9c8dab5e6791c93b91327
agnieszka <273017@student.pwr.edu.pl>
2025-07-21 11:46:22 +0200

    fetch cards fo review and shuffle them

--- frontend/src/components/ReviewView/ReviewView.tsx
+++ frontend/src/components/ReviewView/ReviewView.tsx
@@ -1,22 +1,52 @@
+import { useEffect, useState } from "react";
 import ReviewCard from "./ReviewCard/ReviewCard";
 import useStyles from "./ReviewView.styles";
+import type { CardProps } from "../EditView/Card/CardProps.types";
+import { useParams } from "react-router-dom";
+import { shuffleCards } from "../../services/CardReviewService";
 
 function ReviewView() {
 
+    const DB_ADDRESS = import.meta.env.VITE_DB_ADDRESS;
+    const CARDS = import.meta.env.VITE_CARDS;
     const styles = useStyles();
+    const params = useParams();
+    const [cards, setCards] = useState<CardProps[]>();
+    const [currentCardIndex, setcurrentCardIndex] = useState(0);
+
+    useEffect(() => {
+        if (params) {
+            fetchCards();
+        }
+    }, [params]);
+
+    const fetchCards = async () => {
+        try {
+            const res = await fetch(`${DB_ADDRESS}${CARDS}?search=${params.id}`);
+            const fetchedCards = await res.json();
+            if (fetchedCards) {
+                prepareCards(fetchedCards)
+            }
+        } catch (err) {
+            console.error('Error:', err);
+        }
+    };
+
+    function prepareCards(cards: CardProps[]) {
+        const shuffledCards = shuffleCards(cards)
+        setCards(shuffledCards)
+    }
 
     function handleRememberedCard(_id: number) {
-
     }
 
 
     function handleNotRememberedCard(_id: number) {
-
     }
 
     return (
         <div className={styles.mainContainer}>
-            <ReviewCard _id={1} word="cat" translation="kot" handleYes={handleRememberedCard} handleNo={handleNotRememberedCard} ></ReviewCard>
+            {cards?.length ? <ReviewCard _id={cards[currentCardIndex]._id} word={cards[currentCardIndex].word} translation={cards[currentCardIndex].translation} handleYes={handleRememberedCard} handleNo={handleNotRememberedCard} /> : null}
         </div>
     )
 }
