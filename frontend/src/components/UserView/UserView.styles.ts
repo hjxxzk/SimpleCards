dd2a40e0258b837adfd4c4f98faa4d149d167555
agnieszka <273017@student.pwr.edu.pl>
2025-08-01 14:31:49 +0200

    add logOut Button

---
+++ frontend/src/components/UserView/UserView.styles.ts
@@ -0,0 +1,5 @@
+export const useStyles = () => {
+    return {
+        logOutButton: "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex gap-5 border-1 border-gray-300 rounded-2xl p-6 hover:bg-gray-300 cursor-pointer"
+    };
+};
